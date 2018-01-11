3820dbb626a2d61ec47dc8e44fb53ef5df858aee
Nahal Esmaeili <nesmaeili@myseneca.ca>
2018-01-11 14:32:47 -0500

    Add files via upload

--- chatApp/Client.java
+++ chatApp/Client.java
@@ -11,18 +11,18 @@ public class Client {
 		System.out.print("Enter your name: ");
 		Scanner sc = new Scanner(System.in);
     String name = sc.nextLine();
-		/** InputStreamReader converts byte streams to character streams */
-		/** BufferedReader class reads text from a character-input stream, buffering characters
+		/* InputStreamReader converts byte streams to character streams */
+		/* BufferedReader class reads text from a character-input stream, buffering characters
 		so as to provide for the efficient reading of characters, arrays, and lines */
-		// wrap the input stream with a BufferedReader constructor
+		/** wrap the input stream with a BufferedReader constructor */
 		BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-		/** Class PrintWriter Prints formatted representations of objects to a text-output stream */
-		// wrap the output stream with a PrintWriter constructor
-		// true as the second argument specifies that we want to automatically flush the output stream
+		/* Class PrintWriter Prints formatted representations of objects to a text-output stream */
+		/** wrap the output stream with a PrintWriter constructor */
+		/* true as the second argument specifies that we want to automatically flush the output stream */
 		PrintWriter output = new PrintWriter(getSocket().getOutputStream(), true);
 		while (true) {
 			System.out.print("Enter Text: ");
-			/** readLine() is a member of the BufferedReader class that reads a string from the keyboard */
+			/* readLine() is a member of the BufferedReader class that reads a string from the keyboard */
 			String message = input.readLine();
 			output.println(name + ": " + message);
 		if(message.equals("exit")) {
--- chatApp/Server.java
+++ chatApp/Server.java
@@ -14,27 +14,27 @@ public class Server {
 
 	public void runServer() throws IOException {
 		Date date = new Date();
-		// Create a server socket
+		/** Create a server socket */
 		ServerSocket serverSocket = new ServerSocket(PORT);
 		System.out.println("MultiThreadServer started at " + date);
-		// loop for accepting unlimited number of clients
+		/** Loop for accepting unlimited number of clients */
 		while (true) {
-			// accept client socket
+			/** Listen and accept client socket */
 			Socket socket = serverSocket.accept();
-			/** Thread.start() is required to actually create a new thread
+			/* Thread.start() is required to actually create a new thread
 			 so that the runnable's run method is executed in parallel*/
-			// start a new thread and calls the run() method for the accepted socket
+			/** Start a new thread and calls the run() method for the accepted socket */
 			new ServerThread(socket).start();
 		}
 	}
 
 	public class ServerThread  extends Thread {
 		Socket socket;
-		// Construct a thread
+		/** Construct a thread */
 		ServerThread(Socket socket) {
 			this.socket = socket;
 		}
-		// Implement run() method which is the entry point of the new thread
+		/** Implement run() method which is the entry point of the new thread */
 		public void run() {
 			try {
 				System.out.println("Connection from " + socket + " at " + new Date());
